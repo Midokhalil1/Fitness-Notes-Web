b884e81f9a7ffddcbc006eabd5307fe6f7993287
Midokhalil1 <midokhalil22@gmail.com>
2022-11-26 12:11:21 -0500

    fetching

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,39 @@
 
-import Hero from "../components/Hero";
+import { useEffect, useState } from "react";
+// import Hero from "../components/Hero";
 
+// Fetching data from the backend 
+const Home = () => {
+    const [notes, setNotes] = useState(null) // if the response is not OK send NULL 
 
-export default function Home() {
+    useEffect(() => {
+        const fetchNotes = async () => {
+            const response = await fetch('/api/notes') //fetching 
+            const json = await response.json() //passing JSON
+
+            if (response.ok) {
+                setNotes(json)   //is the response is ok pass the json
+            }
+
+        }
+        fetchNotes()
+
+    }, [])
+
     return (
-        <>
-        <Hero />
-       
-        </>
+        <div className="home">
+            <div className="notes">
+                {notes && notes.map((note) => (
+                    <p key={note._id}>{note.title}</p>
+
+                ))}
+            </div>
+
+
+
+        </div>
     )
+
 }
+
+
