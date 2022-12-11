93cdb0137557dd564e6956a49aaa7fc20d7dec44
Midokhalil1 <midokhalil22@gmail.com>
2022-12-10 21:07:33 -0500

    updated

--- src/components/ExerciseNotes.jsx
+++ src/components/ExerciseNotes.jsx
@@ -3,8 +3,9 @@ import { Checkbox } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 // tell React that your component needs to do something after render
 import { Card } from "antd";
-import { EditOutlined, DeleteOutlined, PlusOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined  } from "@ant-design/icons";
 import logo1 from "../assets/logo1.png";
+import { Overlay } from "antd/es/popconfirm/PurePanel";
 
 const { Meta } = Card;
 export default function ExerciseNotes({ notes, setNotes }) {
@@ -70,14 +71,16 @@ export default function ExerciseNotes({ notes, setNotes }) {
               key={note._id}
               title={note.title}
               style={{
-                width: 300,
+                padding: 3,
               }}
               actions={[
                 // <PlusOutlined key="add" />,
-                // <EditOutlined  className="edit-button"   key="edit" />,
-                <DeleteOutlined
-                  className="edit-button"
+                <EditOutlined    key="edit" />,
+                <DeleteOutlined 
+                hoverable
+                  
                   key="delete"
+         
                   onClick={() => deleteNotes(note._id)}
                 />,
               ]}
@@ -87,15 +90,15 @@ export default function ExerciseNotes({ notes, setNotes }) {
                   onChange={(e) => updateNote(e, note._id)}
                   checked={note?.complete}
                 >
-                  Checkbox{" "}
+                  Complete {" "}
                 </Checkbox>
                 <p className="day-color">DAY: {note.day}</p>{" "}
                 <p className="day-color">DATE: {note.date}</p>
                 
                 <br />
                 <p>RUN: {note.runs}</p>
-                <p>CYCLE: {note.cycling}</p>
-                <p>SWIM: {note.swimming}</p>
+                <p>CYCLE: {note.cycle}</p>
+                <p>SWIM: {note.swim}</p>
                 <p>GYM: {note.gym}</p>
               </section>
 
@@ -109,3 +112,5 @@ export default function ExerciseNotes({ notes, setNotes }) {
     </div>
   );
 }
+
+
